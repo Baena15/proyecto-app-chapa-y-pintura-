4bc7696f444ff7ba2eb34f84ece84e0bd1e2b436
Baena15 <ismael.alfaromarin@icloud.com>
2026-04-27 19:44:53 +0200

    fix: configure GitHub Pages deploy via GitHub Actions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/proyecto-app-chapa-y-pintura-/',
   plugins: [
     react(),
     VitePWA({
